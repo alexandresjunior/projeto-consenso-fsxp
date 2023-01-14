59e5d13ed97a7db286b9d7a4197a0eea734727ac
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2023-01-14 20:35:10 -0300

    consertado

--- Aula_00_Java/02/Exer06/Dependente.java
+++ Aula_00_Java/02/Exer06/Dependente.java
@@ -3,13 +3,9 @@ package Exer06;
 import java.sql.Date;
 
 public class Dependente {
-    private int funcionarios;
     private int sequencial;
     private String nomeCompleto;
     private Date dataNascimento;
     private Integer sexo;
     private boolean participaPlano;
-    public Dependente(){
-        funcionarios++;
-    }
 }
