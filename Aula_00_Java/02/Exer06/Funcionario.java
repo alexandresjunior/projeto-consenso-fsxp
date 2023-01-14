527c1b0c355ff340950bfaac378e92359a7ceca5
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2023-01-14 20:34:33 -0300

    consertado

--- Aula_00_Java/02/Exer06/Funcionario.java
+++ Aula_00_Java/02/Exer06/Funcionario.java
@@ -3,10 +3,10 @@ package Exer06;
 import java.sql.Date;
 
 public class Funcionario {
-    Dependente d = new Dependente();
     private Integer matricula, sexo;
     private String nomeCompleto, cpf;
     private Date dataNascimento;
     private double salario;
     private int telefone;
+    private List<Dependente> dependentes;
 }
