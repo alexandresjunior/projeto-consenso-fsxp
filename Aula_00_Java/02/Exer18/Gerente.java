eeef8db911e78d95bcb83d92801b69ba33895047
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2023-01-17 11:18:32 -0300

    Add files via upload

--- Aula_00_Java/02/Exer18/Gerente.java
+++ Aula_00_Java/02/Exer18/Gerente.java
@@ -6,5 +6,8 @@ public class Gerente extends Pessoa{
     public void listaFuncion(){}
     public void listaCliente(){}
     public void criarFuncionario(){}
-    
+    private Funcionario funcionario;
+    private PedidoBasico pedidoBasico;
+    public void add(Gerente gerente) {
+    }
 }
